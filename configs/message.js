819a47a433a99c73f2d05cac9ecdcd019641eca9
JordaneTakoukam <takoukam.jordane@gmail.com>
2024-02-05 18:28:46 +0100

    lundi 05-02-2024 18h28

--- configs/message.js
+++ configs/message.js
@@ -1,20 +1,64 @@
 const message = {
-    erreurServeur: 'Erreur interne du serveur !',
-    emailExiste: "Cette adresse e-mail est déjà enregistrée !",
-    inscriptReuissie: "Inscription éffectuer avec succès !",
-    connexionReussie: "Connexion éffectuer avec succès !",
-    userNonTrouver: 'Utilisateurs non trouvé !',
-    motDePasseIncorrect: "Mot de passe incorrect !",
-    tokenAccesNonAutoriser: 'Accès non autorisé. Token manquant !',
-    roleAccesNonAutoriser: 'Accès non autorisé. Rôle insuffisant !',
-    invalidToken: 'Token invalide !',
-    // company
-    companyCreer: "Company créée avec succès !",
-    clientCreer: "Client créée avec succès !",
-    clientNonTrouve: "Client non trouver !",
-    clientSupprime: "Client supprimer !",
-    clientMaj: "Client mis a jour !",
-    motDePasseChange: "Mot de passe changer avec succes !"
+    erreurServeur: {
+        fr: "Erreur interne du serveur !",
+        en: "Internal server error!"
+    },
+    emailExiste: {
+        fr: "Cette adresse e-mail est déjà enregistrée !",
+        en: "This email address is already registered!"
+    },
+    inscriptReuissie: {
+        fr: "Inscription effectuée avec succès !",
+        en: "Registration successful!"
+    },
+    connexionReussie: {
+        fr: "Connexion effectuée avec succès !",
+        en: "Login successful!"
+    },
+    userNonTrouver: {
+        fr: "Utilisateur non trouvé !",
+        en: "User not found!"
+    },
+    motDePasseIncorrect: {
+        fr: "Mot de passe incorrect !",
+        en: "Incorrect password!"
+    },
+    tokenAccesNonAutoriser: {
+        fr: "Accès non autorisé. Token manquant !",
+        en: "Unauthorized access. Missing token!"
+    },
+    roleAccesNonAutoriser: {
+        fr: "Accès non autorisé. Rôle insuffisant !",
+        en: "Unauthorized access. Insufficient role!"
+    },
+    invalidToken: {
+        fr: "Token invalide !",
+        en: "Invalid token!"
+    },
+    companyCreer: {
+        fr: "Company créée avec succès !",
+        en: "Company created successfully!"
+    },
+    clientCreer: {
+        fr: "Client créé avec succès !",
+        en: "Client created successfully!"
+    },
+    clientNonTrouve: {
+        fr: "Client non trouvé !",
+        en: "Client not found!"
+    },
+    clientSupprime: {
+        fr: "Client supprimé !",
+        en: "Client deleted!"
+    },
+    clientMaj: {
+        fr: "Client mis à jour !",
+        en: "Client updated!"
+    },
+    motDePasseChange: {
+        fr: "Mot de passe changé avec succès !",
+        en: "Password changed successfully!"
+    }
 };
 
 export { message };
